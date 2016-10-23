9dcb3c0ca5b28abdc87787bc221e7d4e7e02487b
Johnny Gu <johnny@wiredcraft.com>
2016-10-23 23:34:06 +0800

    added package file

--- Package.swift
+++ Package.swift
@@ -3,6 +3,5 @@ import PackageDescription
 let package = Package(
     name: "CorsMiddleware",
     dependencies: [
-	.Package(url: "https://github.com/jhonny-me/CorsMiddleware.git", majorVersion: 1)
     ]
 )
